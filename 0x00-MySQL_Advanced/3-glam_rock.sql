9ca07668137cce4233a6820d2e467ecf30119973
Elizabeth Mashilo <98324930+LizzMash@users.noreply.github.com>
2022-12-15 09:31:43 +0200

    Update 3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,9 @@
-SELECT band_name, IF(split is NULL , 2020 - formed, (split - formed))  lifespan
-SELECT brand_name,
-       COALESCE(split, 2020) - formed AS lifespan FROM metal_bands
+-- List all bands with Glam rock as their main style
+
+SELECT band_name,
+CASE WHEN split is NULL THEN
+	(YEAR(CURDATE()) - formed) ELSE 
+	(split - formed) END AS lifespan
 FROM metal_bands
-WHERE style LIKE '%Glam rock%'
+WHERE FIND_IN_SET('Glam rock', style)
 ORDER BY lifespan DESC;
